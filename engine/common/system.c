69e9e56300eac24a1086206eb76f160f5f3a3f69
Marlon Beijer <marlon@eevul.org>
2023-08-03 20:53:40 +0200

    More Amiga fixes

--- engine/common/system.c
+++ engine/common/system.c
@@ -138,7 +138,7 @@ const char *Sys_GetCurrentUser( void )
 	sceAppUtilSystemParamGetString( SCE_SYSTEM_PARAM_ID_USERNAME, username, sizeof( username ) - 1 );
 	if( COM_CheckStringEmpty( username ))
 		return username;
-#elif XASH_POSIX && !XASH_ANDROID && !XASH_NSWITCH
+#elif XASH_POSIX && !XASH_ANDROID && !XASH_NSWITCH && !XASH_AMIGA
 	uid_t uid = geteuid();
 	struct passwd *pw = getpwuid( uid );
 
